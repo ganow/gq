c842231ca9477eaba438615015d6ee720cbf45f9
Yoshihiro Nagano <y.nagano.92@gmail.com>
2015-03-02 18:25:28 +0900

    bugfix in seupt

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires = open('requirements.txt').read().splitlines(),
     packages=['gq'],
     classifiers = [
-        'Programming Language :: Python :: 2.7.7',
+        'Programming Language :: Python :: 2 :: Only',
         'License :: OSI Approved :: MIT License'
     ]
 )
